ac842588b939512f0dbee7fffcb1576a9eff7afd
Daniel Enríquez Barrera <daniel@cbqasolutions.com>
2018-11-16 18:30:56 -0600

    dashboard advance

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { clearCurrentProfile } from "./actions/profileActions";
+import PrivateRoute from "./components/common/PrivateRoute";
 //layout components
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
@@ -12,6 +14,9 @@ import Footer from "./components/layout/Footer";
 //auth components
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/create-profile/CreateProfile";
+import EditProfile from "./components/edit-profile/EditProfile";
 
 import "./App.css";
 import { decode } from "punycode";
@@ -24,13 +29,12 @@ if (localStorage.jwtToken) {
   const decoded = jwt_decode(token);
   //set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
-
   //check for expired token
   const currentTime = Date.now() / 1000;
   if (decode.exp < currentTime) {
     store.dispatch(logoutUser());
     //TODO: Clear current profile
-
+    store.dispatch(clearCurrentProfile());
     //redirect to login
     window.location.href = "/login";
   }
@@ -47,6 +51,23 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/edit-profile"
+                  component={EditProfile}
+                />
+              </Switch>
             </div>
             <Footer />
           </div>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
+import { clearCurrentProfile } from "../../actions/profileActions";
 
 class Navbar extends Component {
   onLogoutClick(e) {
     e.preventDefault();
+    this.props.clearCurrentProfile();
     this.props.logoutUser();
   }
   render() {
@@ -15,13 +17,18 @@ class Navbar extends Component {
     const authLinks = (
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
+          <Link className="nav-link" to="/dashboard">
+            Dashboard
+          </Link>
+        </li>
+        <li className="nav-item">
           <a
-            href=""
+            href="test"
             onClick={this.onLogoutClick.bind(this)}
             className="nav-link"
           >
             <img
-            className="rounded-circle"
+              className="rounded-circle"
               style={{ width: "25px", marginRight: "5px" }}
               src={user.avatar}
               alt={user.name}
@@ -91,5 +98,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { logoutUser }
+  { logoutUser, clearCurrentProfile }
 )(Navbar);
