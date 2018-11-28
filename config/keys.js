be4bdfb1d32749bd6a844169f754cafa37e7f0bd
Daniel Enríquez Barrera <daniel@cbqasolutions.com>
2018-11-28 11:13:58 -0600

    keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://daniel:123asdZXC@ds133358.mlab.com:33358/mern-course",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
